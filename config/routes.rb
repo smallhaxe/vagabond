97742d262cc659a952ae58c53b3abd8597d62e8c
Ugo Aniukwu <ugo@smallhaxe.com>
2015-06-15 16:23:45 -0700

    unconflicting again

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,15 @@
 Rails.application.routes.draw do
   
-<<<<<<< HEAD
  root to: "welcome#index"
 
-  get "/welcome", to: "welcome#index"
-=======
-  root to: "welcome#index"
+ resources :posts
+ 
+ resources :users, except: [:new]
+ get "/sign_up", to: "user#new"
+ 
+ resources :places
+ 
+ get "/login", to: "sessions#new"
+ post "/login", to: "sessions#create"
   
-  resources :posts
-  
-  resources :users, except: [:new]
-  get "/sign_up", to: "user#new"
-  
-  resources :places
-  
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-  
->>>>>>> 6ea2aa9bdf39529a3113ddb80195bd04c5d48450
 end

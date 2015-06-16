e4f3052cc5fd93d38d28953e49bc4954ef13cc34
Ugo Aniukwu <ugo@smallhaxe.com>
2015-06-15 18:38:52 -0700

    ffaker + seed

--- Gemfile
+++ Gemfile
@@ -43,5 +43,9 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+  # FFaker for seed data
+  gem 'ffaker', '~> 2.0.0'
+  
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'ffaker'
+
+5.times do |i|
+  User.create(first_name: FFaker::Name::first_name, last_name: FFaker::Name::last_name)
+end

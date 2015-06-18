d32c0a2b49803e8386fd96112f62c8851973a8fe
Ugo Aniukwu <ugo@smallhaxe.com>
2015-06-18 12:24:45 -0700

    integrating blockspring in places#index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150618160248) do
+ActiveRecord::Schema.define(version: 20150618180937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20150618160248) do
     t.string   "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal  "lat"
+    t.decimal  "lng"
   end
 
   create_table "posts", force: :cascade do |t|

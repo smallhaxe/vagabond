4f2b1648faa83121322aa0192eecc358c5366ac4
tejaswisagiraju <tejaswi.sagiraju@gmail.com>
2015-06-18 10:08:35 -0700

    updated users_controller
    deleted the password confirmation line

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     user_params = params.require(:user).permit(:first_name, :last_name, :email, :password, :current_city)
     user = User.new(user_params)
     
-    if (user.save && user.password == user.password_confirmation)
+    if user.save
       login user
 
       redirect_to "/users/#{user.id}"
